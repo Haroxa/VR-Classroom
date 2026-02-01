c0c9bfcf433761ef9a5ecb2d472db9ff82cccb67
Haroxa <2379008409@qq.com>
2026-02-01 10:05:32 +0800

    init project

---
+++ vr-classroom-backend/src/main/java/com/university/vrclassroombackend/VrClassroomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.university.vrclassroombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrClassroomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrClassroomBackendApplication.class, args);
+    }
+
+}
---
+++ vr-classroom-backend/src/test/java/com/university/vrclassroombackend/VrClassroomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.vrclassroombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrClassroomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
